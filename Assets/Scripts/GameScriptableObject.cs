a4f7fc800b43ffdff94fd881c9a1788d958a7205
Jeff <1571098645@qq.com>
2020-09-23 11:39:59 +0800

    【ScriptableObject】

---
+++ Assets/Scripts/GameScriptableObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GameScriptableObject : ScriptableObject
+{
+
+    [SerializeField]
+    public List<Enemy> enemies;
+
+    [System.Serializable]
+    public class Enemy
+    {
+        public int id;
+        public string name;
+    }
+}
